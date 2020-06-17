1cc8af55a3a9d89385b963fe434a1eeab70d2750
Jürgen Weber <weberjn@jwi.de>
2020-06-17 09:29:20 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace FollowWindow
+{
+    public partial class Form1 : Form
+    {
+        private System.IntPtr notepadhWnd;
+        private IntPtr hWinEventHook;
+        private Process targetProc;
+        private RECT rect = new RECT();
+        protected Hook.WinEventDelegate WinEventDelegate;
+        static GCHandle GCSafetyHandle;
+
+        public Form1()
+        {
+            InitializeComponent();
+            WinEventDelegate = new Hook.WinEventDelegate(WinEventCallback);
+            GCSafetyHandle = GCHandle.Alloc(WinEventDelegate);
+
+            try
+            {
+                targetProc = Process.GetProcessesByName("notepad").FirstOrDefault(p => p != null);
+                if (targetProc != null)
+                {
+                    notepadhWnd = targetProc.MainWindowHandle;
+                    uint targetThreadId = Hook.GetWindowThread(notepadhWnd);
+
+                    if (notepadhWnd != IntPtr.Zero)
+                    {
+                        hWinEventHook = Hook.WinEventHookOne(Hook.SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE,
+                                                             WinEventDelegate,
+                                                             (uint)targetProc.Id,
+                                                             targetThreadId);
+                        rect = Hook.GetWindowRect(notepadhWnd);
+                        this.Location = new System.Drawing.Point(rect.Right, rect.Top);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //ErrorManager.Logger(this, this.InitializeComponent(), ex.HResult, ex.Data, DateTime.Now);
+                throw ex;
+            }
+        }
+
+        protected void WinEventCallback(IntPtr hWinEventHook,
+                                        Hook.SWEH_Events eventType,
+                                        IntPtr hWnd,
+                                        Hook.SWEH_ObjectId idObject,
+                                        long idChild,
+                                        uint dwEventThread,
+                                        uint dwmsEventTime)
+        {
+            if (hWnd == notepadhWnd &&
+                eventType == Hook.SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE &&
+                idObject == (Hook.SWEH_ObjectId)Hook.SWEH_CHILDID_SELF)
+            {
+                rect = Hook.GetWindowRect(hWnd);
+                this.Location = new Point(rect.Right, rect.Top);
+                this.labelPos.Text = this.Location.ToString();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GCSafetyHandle.Free();
+            Hook.WinEventUnhook(hWinEventHook);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (targetProc == null)
+            {
+                this.Hide();
+                MessageBox.Show("Notepad not found!", "Target Missing", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                this.Close();
+            }
+            else
+            {
+                this.Size = new Size(50, 140);
+            }
+        }
+
+        private Label label1;
+        private Label labelPos;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FollowWindow
+{
+    public partial class Form1 : Form
+    {
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "labelHello";
+            // 
+            // labelPos
+            // 
+            this.labelPos.AutoSize = true;
+            this.labelPos.Location = new System.Drawing.Point(77, 173);
+            this.labelPos.Name = "labelPos";
+            this.labelPos.Size = new System.Drawing.Size(35, 13);
+            this.labelPos.TabIndex = 1;
+            this.labelPos.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 260);
+            this.Controls.Add(this.labelPos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
